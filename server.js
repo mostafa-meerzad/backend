e5821d17770210caf696d12da9b9629b4d61c8d4
mostafa <mostafameerzad@gmail.com>
2024-08-06 17:32:12 +0430

    create user's model and run the server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017");
+
+const User = require("./models/user");
+
 const app = express();
 // middleware
 app.use(express.json());
@@ -8,6 +12,28 @@ app.use(express.json());
 // simple route
 app.get("/", (req, res) => {
   return res.send("Hello Node.js World!");
+});
+
+// get all users
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
+// create a new user
+app.post("/api/users", async (req, res) => {
+  const user = new User({ name: req.body.name, email: req.body.email });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
 });
 
 // start server
